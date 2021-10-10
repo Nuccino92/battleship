a6f07fa8d120cc13b2fab2c5e0595074665f6314
Anthony Nucci <anthnucci@hotmail.com>
2021-10-10 03:05:35 -0400

    created all ships using Ship factory

---
+++ src/modules/fleet.js
@@ -0,0 +1,18 @@
+import Ship from "./ship";
+
+// eslint-disable-next-line import/prefer-default-export
+function fleet() {
+  const Carrier = new Ship("Carrier", 5);
+
+  const Battleship = new Ship("Battleship", 4);
+
+  const Cruiser = new Ship("Cruiser", 3);
+
+  const Submarine = new Ship("Submarine", 3);
+
+  const Destroyer = new Ship("Destroyer", 2);
+
+  return { Carrier, Battleship, Cruiser, Submarine, Destroyer };
+}
+
+export default fleet;
