bd3bea50d8f8fe55088c6c75409fe42b4459045b
Anthony Nucci <anthnucci@hotmail.com>
2021-10-10 03:06:15 -0400

    made tests for the Ship function using a testShip

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,1 +1,39 @@
 /* eslint-disable no-undef */
+// eslint-disable-next-line import/no-extraneous-dependencies
+
+import Ship from "../modules/ship";
+
+const testShip = new Ship("Battleship", 4);
+
+describe("Created Ships", () => {
+  describe("properties", () => {
+    test("check ship name", () => {
+      expect(testShip.name).toBe("Battleship");
+    });
+
+    test("check ship length", () => {
+      expect(testShip.length).toBe(4);
+    });
+  });
+});
+
+describe("Hit/Sunk functionality", () => {
+  test("the ship has hits array", () => {
+    expect(testShip.hits).toEqual([null, null, null, null]);
+  });
+  test("check working hit function", () => {
+    testShip.hit(1);
+    expect(testShip.hits).toEqual([null, "hit", null, null]);
+  });
+
+  test("working isShipSunk status", () => {
+    expect(testShip.isShipSunk).toBe(false);
+  });
+  test("sink the ship", () => {
+    testShip.hit(0);
+    testShip.hit(2);
+    testShip.hit(3);
+    testShip.isSunk();
+    expect(testShip.isShipSunk).toBe(true);
+  });
+});
