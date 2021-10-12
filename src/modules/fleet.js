f67764df536479fca3c16b206b51b9acf2e1b52a
Anthony Nucci <anthnucci@hotmail.com>
2021-10-12 03:10:07 -0400

    .

--- src/modules/fleet.js
+++ src/modules/fleet.js
@@ -2,15 +2,15 @@ import Ship from "./ship";
 
 // eslint-disable-next-line import/prefer-default-export
 function fleet() {
-  const Carrier = new Ship("Carrier", 5);
+  const Carrier = () => new Ship("Carrier", 5);
 
-  const Battleship = new Ship("Battleship", 4);
+  const Battleship = () => new Ship("Battleship", 4);
 
-  const Cruiser = new Ship("Cruiser", 3);
+  const Cruiser = () => new Ship("Cruiser", 3);
 
-  const Submarine = new Ship("Submarine", 3);
+  const Submarine = () => new Ship("Submarine", 3);
 
-  const Destroyer = new Ship("Destroyer", 2);
+  const Destroyer = () => new Ship("Destroyer", 2);
 
   return { Carrier, Battleship, Cruiser, Submarine, Destroyer };
 }
