977f901935cb25ebd10c4438017f06d6fb3241cd
Anthony Nucci <anthnucci@hotmail.com>
2021-10-16 02:53:54 -0400

    grabs dom elements

---
+++ src/modules/displayController/domElements.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
+export const domElements = {
+  playerGameboard: document.querySelector(".player-board"),
+  computerGameboard: document.querySelector(".computer-board"),
+};
