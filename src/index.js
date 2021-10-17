476dc6ce734daf131e4434c16dd77efda9403e5c
Anthony Nucci <anthnucci@hotmail.com>
2021-10-17 01:22:53 -0400

    added event listener for the grid

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ import "./styles/main.css";
 const game = Game();
 
 game.renderGameboards();
+
+game.renderShips();
+
+const grid = document.querySelectorAll(".grid-cell");
+
+grid.forEach((cell) =>
+  cell.addEventListener(
+    "click",
+    (e) => {
+      game.boardAttack(e);
+    },
+    { once: true }
+  )
+);
