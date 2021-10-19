94dc67c4c0226207f7fe519bfeb2dc5af56c69d7
Anthony Nucci <anthnucci@hotmail.com>
2021-10-19 03:14:55 -0400

    added ships array of objects/match ship function

--- src/modules/fleet.js
+++ src/modules/fleet.js
@@ -1,5 +1,11 @@
 import Ship from "./ship";
 
+const Carrier = new Ship("Carrier", 5);
+const Battleship = new Ship("Battleship", 4);
+const Cruiser = new Ship("Cruiser", 3);
+const Submarine = new Ship("Submarine", 3);
+const Destroyer = new Ship("Destroyer", 2);
+
 // eslint-disable-next-line import/prefer-default-export
 function fleet() {
   const Carrier = () => new Ship("Carrier", 5);
@@ -15,4 +21,16 @@ function fleet() {
   return { Carrier, Battleship, Cruiser, Submarine, Destroyer };
 }
 
-export default fleet;
+const ships = [Carrier, Battleship, Cruiser, Submarine, Destroyer];
+
+function matchShip(name) {
+  let matchedShip;
+  ships.forEach((ship) => {
+    if (ship.name === name) {
+      matchedShip = ship;
+    }
+  });
+  return matchedShip;
+}
+
+export { fleet, ships, matchShip };
