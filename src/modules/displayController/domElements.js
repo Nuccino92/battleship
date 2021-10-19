ee18d7b71fde099a5da997d2cc2cf353ef801f7c
Anthony Nucci <anthnucci@hotmail.com>
2021-10-19 03:11:48 -0400

    added dom functionality

--- src/modules/displayController/domElements.js
+++ src/modules/displayController/domElements.js
@@ -2,4 +2,20 @@
 export const domElements = {
   playerGameboard: document.querySelector(".player-board"),
   computerGameboard: document.querySelector(".computer-board"),
+  shipSetupContainer: document.querySelector(".ship-setup-container"),
+  rotateButton: document.querySelector(".rotate-ships"),
+  shipCell: document.querySelector(".ship-cell"),
 };
+
+export function toggleHorizontalClass() {
+  document.querySelector(".Carrier-container").classList.toggle("horizontal");
+  document
+    .querySelector(".Battleship-container")
+    .classList.toggle("horizontal");
+  document.querySelector(".Cruiser-container").classList.toggle("horizontal");
+  document.querySelector(".Submarine-container").classList.toggle("horizontal");
+  document.querySelector(".Destroyer-container").classList.toggle("horizontal");
+  document
+    .querySelector(".ship-setup-container")
+    .classList.toggle("horizontal");
+}
