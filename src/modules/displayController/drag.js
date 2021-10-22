8011d900ec6e9e020a5836a45dd9bf87242e4bbe
Anthony Nucci <anthnucci@hotmail.com>
2021-10-22 15:48:45 -0400

    finished drag drop functionality

--- src/modules/displayController/drag.js
+++ src/modules/displayController/drag.js
@@ -1,25 +1,31 @@
 import { matchShip } from "../fleet";
+import Gameboard from "../gameboard";
 import { domElements } from "./domElements";
+import renderController from "./renderController";
 
 function Drag(playerBoard) {
-  const ships = document.querySelectorAll(".ship");
-  const cells = domElements.playerGameboard.childNodes;
+  function dragEventListeners() {
+    const ships = document.querySelectorAll(".ship");
+    const cells = document.querySelectorAll(".grid-cell");
 
-  ships.forEach((ship) => ship.addEventListener("dragstart", dragStart));
-  cells.forEach((cell) => cell.addEventListener("dragover", dragOver));
-  cells.forEach((cell) => cell.addEventListener("dragenter", dragEnter));
-  cells.forEach((cell) => cell.addEventListener("dragleave", dragLeave));
-  cells.forEach((cell) => cell.addEventListener("drop", dragDrop));
-  cells.forEach((cell) => cell.addEventListener("dragend", dragEnd));
+    ships.forEach((ship) => ship.addEventListener("dragstart", dragStart));
+
+    cells.forEach((cell) => cell.addEventListener("dragover", dragOver));
+    cells.forEach((cell) => cell.addEventListener("dragenter", dragEnter));
+    cells.forEach((cell) => cell.addEventListener("dragleave", dragLeave));
+    cells.forEach((cell) => cell.addEventListener("drop", dragDrop));
+    cells.forEach((cell) => cell.addEventListener("dragend", dragEnd));
+
+    ships.forEach((ship) =>
+      ship.addEventListener("mousedown", (e) => {
+        // eslint-disable-next-line no-use-before-define
+        selectedShipNameWithIndex = e.target;
+      })
+    );
+  }
 
   let selectedShipNameWithIndex;
   let draggedShip;
-
-  ships.forEach((ship) =>
-    ship.addEventListener("mousedown", (e) => {
-      selectedShipNameWithIndex = e.target;
-    })
-  );
 
   function dragStart(e) {
     draggedShip = e.target;
@@ -44,20 +50,36 @@ function Drag(playerBoard) {
     let x = Number(cell.dataset.x);
     let y = Number(cell.dataset.y);
 
-    //gets the correct starting point
+    // gets the correct starting point
     if (ship.direction === "vertical") {
       x = x - index;
     }
-    //gets the correct starting point
+    // gets the correct starting point
     if (ship.direction === "horizontal") {
       y = y - index;
     }
 
-    playerBoard.placeShips(ship, x, y);
+    const placed = playerBoard.placeShips(ship, x, y);
+
+    if (placed) {
+      renderController().renderBoard(domElements.playerGameboard, playerBoard);
+      dragEventListeners();
+      draggedShip.style.display = "none";
+
+      console.log(playerBoard.placedShips);
+
+      // checks if all the 5 ships have been placed
+      if (playerBoard.placedShips.length === 5) {
+        domElements.setupContainer.classList.add("remove");
+        domElements.startButton.classList.add("active");
+      }
+    }
   }
+
   function dragEnd() {
     //
   }
+  return { dragEventListeners };
 }
 
 export default Drag;
