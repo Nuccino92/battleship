2bc953909283c55e8fa85965e669726dd1827271
Anthony Nucci <anthnucci@hotmail.com>
2021-10-24 01:27:48 -0400

    drag drop removes ship containers

--- src/modules/displayController/drag.js
+++ src/modules/displayController/drag.js
@@ -1,20 +1,17 @@
 import { matchShip } from "../fleet";
-import Gameboard from "../gameboard";
+
 import { domElements } from "./domElements";
 import renderController from "./renderController";
 
-function Drag(playerBoard) {
+function Drag(playerBoard, playerFleet) {
   function dragEventListeners() {
     const ships = document.querySelectorAll(".ship");
     const cells = document.querySelectorAll(".grid-cell");
 
     ships.forEach((ship) => ship.addEventListener("dragstart", dragStart));
-
     cells.forEach((cell) => cell.addEventListener("dragover", dragOver));
     cells.forEach((cell) => cell.addEventListener("dragenter", dragEnter));
-    cells.forEach((cell) => cell.addEventListener("dragleave", dragLeave));
     cells.forEach((cell) => cell.addEventListener("drop", dragDrop));
-    cells.forEach((cell) => cell.addEventListener("dragend", dragEnd));
 
     ships.forEach((ship) =>
       ship.addEventListener("mousedown", (e) => {
@@ -39,17 +36,15 @@ function Drag(playerBoard) {
     e.preventDefault();
   }
 
-  function dragLeave() {
-    console.log("dragleave");
-  }
   function dragDrop(e) {
     const cell = e.target;
-    const ship = matchShip(draggedShip.dataset.name);
+    const { name } = draggedShip.dataset;
+
+    const ship = matchShip(playerFleet, name);
     const index = Number(selectedShipNameWithIndex.dataset.index);
 
     let x = Number(cell.dataset.x);
     let y = Number(cell.dataset.y);
-
     // gets the correct starting point
     if (ship.direction === "vertical") {
       x = x - index;
@@ -62,11 +57,9 @@ function Drag(playerBoard) {
     const placed = playerBoard.placeShips(ship, x, y);
 
     if (placed) {
+      draggedShip.remove();
       renderController().renderBoard(domElements.playerGameboard, playerBoard);
       dragEventListeners();
-      draggedShip.style.display = "none";
-
-      console.log(playerBoard.placedShips);
 
       // checks if all the 5 ships have been placed
       if (playerBoard.placedShips.length === 5) {
@@ -74,10 +67,6 @@ function Drag(playerBoard) {
         domElements.startButton.classList.add("active");
       }
     }
-  }
-
-  function dragEnd() {
-    //
   }
   return { dragEventListeners };
 }
