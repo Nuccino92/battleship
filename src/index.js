8e4b22e8561de1524a30c3bc0b318680288864a3
Anthony Nucci <anthnucci@hotmail.com>
2021-10-26 04:26:04 -0400

    removed grid listener

--- src/index.js
+++ src/index.js
@@ -1,54 +1,49 @@
 import {
   domElements,
   toggleHorizontalClass,
+  startDOMManipulation,
+  chatBoxController,
 } from "./modules/displayController/domElements";
-import { fleet } from "./modules/fleet";
-import Gameboard from "./modules/gameboard";
 import Game from "./modules/gameLoop";
-
 import "./styles/main.css";
 
-const computerBoard = new Gameboard();
 const game = Game();
 
 game.renderGameboards();
 
 game.renderShips();
 
-const grid = document.querySelectorAll(".grid-cell");
-
-grid.forEach((cell) =>
-  cell.addEventListener(
-    "click",
-    (e) => {
-      game.boardAttack(e);
-    },
-    { once: true }
-  )
-);
+game.gridListener();
 
 //rotate button event listener
-
 domElements.rotateButton.addEventListener("click", () => {
   toggleHorizontalClass();
   game.fleetChangeDirection();
 });
 
+// start button listener
 document.querySelector(".start-button").addEventListener("click", () => {
-  domElements.setupContainer.classList.add("remove");
-  domElements.computerGameboard.classList.add("active");
   game.start();
-  domElements.startButton.classList.remove("active");
+  startDOMManipulation();
 });
 
+// replay button listener
 domElements.playAgain.addEventListener("click", () => {
-  domElements.modalContainer.classList.remove("active");
   game.restartGame();
 });
 
-function playAgain() {
-  //restarts the game
-}
-function restartGame() {
-  // restarts the match without setup
-}
+// reset round button listener
+domElements.resetButton.addEventListener(
+  "click",
+  (e) => {
+    chatBoxController("reset");
+    setTimeout(
+      () => {
+        game.restartGame();
+      },
+      3000,
+      { once: false }
+    );
+  },
+  { once: true }
+);
