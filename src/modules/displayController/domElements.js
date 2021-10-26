cd4177b0f3403210c1f4c7bb9bd7acc2725ced85
Anthony Nucci <anthnucci@hotmail.com>
2021-10-26 04:23:24 -0400

    added chatBox/restart & start DOM func

--- src/modules/displayController/domElements.js
+++ src/modules/displayController/domElements.js
@@ -11,7 +11,36 @@ export const domElements = {
   modal: document.querySelector(".modal"),
   playAgain: document.querySelector(".play-again"),
   winner: document.querySelector(".display-winner"),
+  blocker: document.querySelector(".blocker"),
+  crosshairX: document.querySelector(".crosshairX"),
+  crosshairY: document.querySelector(".crosshairY"),
+  chatContainer: document.querySelector(".chat-container"),
+  chatBox: document.querySelector(".chat-box"),
+  chatText: document.querySelector(".chat-text"),
+  resetButton: document.querySelector(".reset-button"),
 };
+
+export function startDOMManipulation() {
+  domElements.setupContainer.classList.add("remove");
+  domElements.computerGameboard.classList.add("active");
+  domElements.startButton.classList.remove("active");
+  domElements.shipSetupContainer.classList.remove("horizontal");
+  domElements.playerGameboard.classList.toggle("transform");
+  domElements.crosshairX.classList.add("active");
+  domElements.crosshairY.classList.add("active");
+  domElements.chatContainer.classList.add("active");
+  domElements.chatText.textContent = "Get ready and fire!";
+}
+
+export function restartDOMManipulation() {
+  domElements.modalContainer.classList.remove("active");
+  domElements.playerGameboard.classList.toggle("transform");
+  domElements.setupContainer.classList.remove("remove");
+  domElements.computerGameboard.classList.remove("active");
+  domElements.crosshairX.classList.remove("active");
+  domElements.crosshairY.classList.remove("active");
+  domElements.chatContainer.classList.remove("active");
+}
 
 export function toggleHorizontalClass() {
   domElements.shipSetupContainer.childNodes.forEach((node) => {
@@ -20,4 +49,61 @@ export function toggleHorizontalClass() {
   document
     .querySelector(".ship-setup-container")
     .classList.toggle("horizontal");
+}
+
+function resetAnimation() {
+  const text = domElements.chatText;
+  text.style.animation = "none";
+  text.offsetHeight;
+  text.style.animation = null;
+}
+
+export function chatBoxController(location, player) {
+  resetAnimation();
+  const text = domElements.chatText;
+
+  function playerHits() {
+    text.textContent = "You hit a ship!";
+    text.style.animation = "printing 1s steps(40, end)";
+  }
+  function playerMissed() {
+    text.textContent = "You missed.";
+    text.style.animation = "printing 1s steps(40, end)";
+  }
+
+  function computerShipSunk() {
+    text.textContent = "You Sunk a Ship!";
+    text.style.animation = "printing 1s steps(40, end)";
+  }
+
+  function computerHits() {
+    text.textContent = "You were Hit!";
+    text.style.animation = "printing 1s steps(40, end)";
+  }
+  function computerMissed() {
+    text.textContent = "They missed.";
+    text.style.animation = "printing 1s steps(40, end)";
+  }
+  function playerShipSunk() {
+    text.textContent = "They sunk a Ship!";
+    text.style.animation = "printing 1s steps(40, end)";
+  }
+
+  if (location === "reset") {
+    text.textContent = "Get back here!";
+    text.style.animation = "printing 1s steps(40, end)";
+    return;
+  }
+
+  if (location === null) {
+    if (player === "human") playerMissed();
+    else computerMissed();
+  }
+  if (location !== null) {
+    if (player === "human") {
+      if (location.isShipSunk === true) computerShipSunk();
+      else playerHits();
+    } else if (location.isShipSunk === true) playerShipSunk();
+    else computerHits();
+  }
 }
